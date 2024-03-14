74c65083936dfe3de3f4b73040d1a535f8ba5eda
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-03-14 03:13:08 +0300

    ✨ feat: add sticky nav feature

--- src/index.js
+++ src/index.js
@@ -15,6 +15,8 @@ const homeLink = document.querySelector('.nav__link--home');
 const menuLink = document.querySelector('.nav__link--menu');
 const contactLink = document.querySelector('.nav__link--contact');
 const contentContainer = document.querySelector('.main__inner');
+const header = document.querySelector('.header');
+const nav = document.querySelector('.nav');
 
 const contents = {
   home: `
@@ -110,12 +112,33 @@ const monitorHash = () => {
   });
 };
 
+const stickyNav = () => {
+  const observerCallback = (payload) => {
+    if (!payload[0].isIntersecting) {
+      nav.classList.add('sticky');
+      return;
+    }
+    nav.classList.remove('sticky');
+  };
+
+  const observerOptions = {
+    threshold: 0,
+  };
+
+  const observeNav = new IntersectionObserver(
+    observerCallback,
+    observerOptions
+  );
+  observeNav.observe(header);
+};
+
 const init = () => {
   let hash = location.hash.slice(1);
   if (!hash) hash = 'home';
   activateNav(hash);
   loadContent(hash);
   monitorHash();
+  stickyNav();
 };
 
 init();
